145d426a0f88273887bc644b7c67180ec9bd63f1
Krishna Manoj PVR <119659348+krishnamanojpvr@users.noreply.github.com>
2026-04-11 22:37:05 +0530

    Improve directory management in buildRepo function
    Refactor buildRepo function to manage site and buildfiles directories more efficiently.

--- .github/build.fish
+++ .github/build.fish
@@ -1,5 +1,4 @@
 #!/usr/bin/env fish
-
 set filename build.config.json
 
 function buildRepo
@@ -10,47 +9,39 @@ function buildRepo
     set repodir ./buildfiles/$name
     set branch "testing"
 
-    # Temp output folder
-    set outputdir ./temp_build
+    if test ! -e site
+        mkdir site
+    else
+        rm -rf ./site/*
+    end
+    if test ! -e buildfiles
+        mkdir buildfiles
+    else
+        rm -rf ./buildfiles/*
+    end
 
-    # Clean temp
-    rm -rf $outputdir
-    mkdir -p $outputdir
-
-    # Clean buildfiles
-    rm -rf buildfiles
-    mkdir buildfiles
-
-    echo "Building: $name"
-    echo "Repo: $repolink"
-
-    # Auth repo
+    echo $name
+    echo $repolink
+    echo $repodir
+    echo "$subdomain $repolink"
     set authrepolink (string replace "https://github.com/" "https://x-access-token:$ACCESS_TOKEN@github.com/" $repolink)
-
     git clone -b $branch "$authrepolink" "$repodir"
-
     cd $repodir
     bun i
     bun run build
     cd -
-
-    # Detect output
-    if test -d $repodir/build
-        echo "Using build/"
-        cp -r $repodir/build/* $outputdir 2>/dev/null
-    else if test -d $repodir/dist
-        echo "Using dist/"
-        cp -r $repodir/dist/* $outputdir 2>/dev/null
-    else
-        echo "❌ No build output found"
-        exit 1
+    mkdir -p "./site/$subdomain"
+    if test -e $repodir/build
+        cp -r $repodir/build/* ./site/$subdomain
+    else if test -e $repodir/dist
+        cp -r $repodir/dist/* ./site/$subdomain
     end
-
-    # Cleanup
-    rm -rf buildfiles
+    rm -r buildfiles
 end
 
 if test "$argv[1]" = "all"
+
+    set filename build.config.json
     set keys (jq -r 'keys_unsorted | .[]' $filename)
     for i in $keys
         buildRepo "$i"
