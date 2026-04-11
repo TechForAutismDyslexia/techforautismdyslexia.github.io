a4a30c6d5a8deb333dd46e3b5095be7b8c9e994a
Krishna Manoj PVR <119659348+krishnamanojpvr@users.noreply.github.com>
2026-04-11 21:54:11 +0530

    Enhance build script with additional logging and error handling

--- .github/build.fish
+++ .github/build.fish
@@ -1,4 +1,5 @@
 #!/usr/bin/env fish
+
 set filename build.config.json
 
 function buildRepo
@@ -9,43 +10,62 @@ function buildRepo
     set repodir ./buildfiles/$name
     set branch "testing"
 
+    # Create site folder
     if test ! -e site
         mkdir site
     else
         rm -rf ./site/$subdomain
     end
+
+    # Clean buildfiles
     if test ! -e buildfiles
         mkdir buildfiles
     else
         rm -rf ./buildfiles/*
     end
 
-    echo $name
-    echo $repolink
-    echo $repodir
-    echo "$subdomain $repolink"
+    echo "Building: $name"
+    echo "Repo: $repolink"
+    echo "Subdomain: $subdomain"
+
+    # Authenticated repo URL
     set authrepolink (string replace "https://github.com/" "https://x-access-token:$ACCESS_TOKEN@github.com/" $repolink)
+
+    # Clone repo
     git clone -b $branch "$authrepolink" "$repodir"
+
     cd $repodir
     bun i
     bun run build
     cd -
+
+    # Create output folder
     mkdir -p "./site/$subdomain"
+
+    # Copy build output safely
     if test -e $repodir/build
-        cp -r $repodir/build/* ./site/$subdomain
+        echo "Using build/ folder"
+        cp -r $repodir/build/* ./site/$subdomain 2>/dev/null
     else if test -e $repodir/dist
-        cp -r $repodir/dist/* ./site/$subdomain
+        echo "Using dist/ folder"
+        cp -r $repodir/dist/* ./site/$subdomain 2>/dev/null
+    else
+        echo "❌ No build output found (build/ or dist/)"
     end
-    rm -r buildfiles
+
+    # Cleanup
+    rm -rf buildfiles
 end
 
+# Entry logic
 if test "$argv[1]" = "all"
 
-    set filename build.config.json
     set keys (jq -r 'keys_unsorted | .[]' $filename)
+
     for i in $keys
         buildRepo "$i"
     end
+
 else
     buildRepo "$argv[1]"
 end
